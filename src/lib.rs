1778481f217d452a4b193994fe2ebf4f75d0e1d4
Joey <joey.xf@gmail.com>
2020-08-23 14:40:51 +0800

    add simple get_row

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 // code copied from https://github.com/rust-dataframe/discussion/issues/7
 #![feature(const_generics)]
 
-use frunk::hlist::{HList, HCons, HNil, Selector, Sculptor};
+// #[macro_use]
+// extern crate frunk;
+// extern crate frunk_core;
 
-#[derive(PartialEq, Eq, Clone)]
+use frunk::hlist::{HList, HCons, HNil, Selector, Sculptor};
+// use frunk::generic::Generic;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
 pub struct Column<T: Copy, const S: &'static str> {
     pub inner: Vec<T>
 }
@@ -19,6 +24,21 @@ impl<T: Copy, const S: &'static str> Column<T, S> {
         self.inner[idx]
     }
 }
+
+// #[derive(Debug, PartialEq)]
+// pub struct Row {
+//     pub col1: i32,
+//     pub col2: f32,
+// }
+
+// impl Row {
+//     pub fn new() -> Self {
+//         Self {
+//             col1: 0,
+//             col2: 0.0,
+//         }
+//     }
+// }
 
 pub struct DataFrame<Col: HList + Clone> {
     inner_cols: Col,
@@ -59,6 +79,14 @@ impl<Col: HList + Clone> DataFrame<Col> {
         target
     }
 
+    pub fn get_row<Index, T1: Copy, const S1: &'static str, T2: Copy, const S2: &'static str>(&self, idx: usize) -> (T1, T2)
+    where Col: Sculptor<HCons<Column<T1, S1>, HCons<Column<T2, S2>, HNil>>, Index>
+    {
+        let (target, _): (HCons<Column<T1, S1>, HCons<Column<T2, S2>, HNil>>, _) = self.inner_cols.clone().sculpt();
+        let list: (Column<T1, S1>, Column<T2, S2>) = target.into();
+        (list.0.inner[idx], list.1.inner[idx])
+    }
+
     pub fn schema(&self) -> Vec<&'static str>
     {
         self.col_names.to_owned()
@@ -78,7 +106,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_add_col() {
+    fn test_dataframe() {
         let df = DataFrame::new();
         let col1: Vec<i32> = vec![1, 2, 3];
         let col2: Vec<f32> = vec![1.1, 2.1, 3.1];
@@ -96,5 +124,40 @@ mod tests {
 
         let cols4 = df.get_column::<_, HCons<Column<i32, "col1">, HNil>>();
         assert_eq!(1, cols4.head.inner[0]);
+
+        let row = df.get_row::<_, i32, "col1", f32, "col2">(0);
+        assert_eq!(1, row.0);
+        assert_eq!(1.1, row.1);
     }
 }
+
+
+// impl<T: Copy, const S: &'static str> Generic for Column<T, S> {
+//     type Repr = HCons<Column<T, S>, HNil>;
+    
+//     fn into(self) -> Self::Repr {
+//         hlist![self]
+//     }
+
+//     fn from(a: Self::Repr) -> Self {
+//         a.head
+//     }
+// }
+
+// impl<T: Copy, const S: &'static str> From<Column<T, S>> for HCons<Column<T, S>, HNil> {
+//     fn from(a: Column<T, S>) -> Self {
+//         hlist![a]
+//     }
+// }
+
+// impl Generic for Row {
+//     type Repr = HCons<Row, HNil>;
+    
+//     fn into(self) -> Self::Repr {
+//         hlist![self]
+//     }
+
+//     fn from(a: Self::Repr) -> Self {
+//         a.head
+//     }
+// }
